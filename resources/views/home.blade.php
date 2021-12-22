3bc692cf4d2c058cd246bfca05aedef03b750d54
rotasistudio <aan.yuliansyah87@gmail.com>
2021-12-22 16:43:31 +0800

    Start belajar laravel via Parsinta

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body>
+    <ul class="navbar">
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact">Contak</a></li>
+        <li><a href="/post">Post</a></li>
+    </ul>
+</body>
+</html>
